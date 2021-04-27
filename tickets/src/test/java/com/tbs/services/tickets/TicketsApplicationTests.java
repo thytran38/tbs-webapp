15cbe3b8fb7702230a8aa828bc7a575517b7b3e3
thytran38 <ax4409h@gmail.com>
2021-04-27 17:20:57 +0700

    Add trips folder, ticket folder, zuul folder

---
+++ tickets/src/test/java/com/tbs/services/tickets/TicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tbs.services.tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
